9ba0ee89cdae14a4d74e316937625dcb6c379e19
TranDong0512 <tdong0512@gmail.com>
2024-11-07 21:49:20 +0700

    ok đi

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
-    outDir: 'dist',     // Thư mục đầu ra là 'dist' ở gốc
+    outDir: 'dist',  // Đầu ra cho thư mục `dist`
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        category: resolve(__dirname, 'src/category.html'),
+        index: resolve(__dirname, 'src/index.html'),  // Tệp HTML chính
+        category: resolve(__dirname, 'src/category.html'), // Thêm các tệp HTML khác
         product_detail: resolve(__dirname, 'src/product_detail.html')
       }
     }
   }
-});
+})
