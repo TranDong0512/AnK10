01daa2195b27ea903a7f7e369a93b3e83ecbded8
TranDong0512 <tdong0512@gmail.com>
2024-11-08 08:42:19 +0700

    ok1

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'src/index.html'),  // Tệp HTML chính
-        category: resolve(__dirname, 'src/category.html'), // Thêm các tệp HTML khác
-        product_detail: resolve(__dirname, 'src/product_detail.html')
+        index: resolve(__dirname, './index.html'),  // Tệp HTML chính
+        category: resolve(__dirname, './category.html'), // Thêm các tệp HTML khác
+        product_detail: resolve(__dirname, './product_detail.html')
       }
     }
   }
